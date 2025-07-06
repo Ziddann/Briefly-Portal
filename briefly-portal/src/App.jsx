25337c5a35eee188342d6595c2d32f33ec4bc1ca
Ziddann <116059932+Ziddann@users.noreply.github.com>
2025-07-06 19:19:31 +0700

    Update

--- briefly-portal/src/App.jsx
+++ briefly-portal/src/App.jsx
@@ -6,10 +6,6 @@ import NewsDetail from './pages/News/NewsDetail';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import Bookmark from './pages/Bookmark';
-import Teknologi from './pages/News/Teknologi';
-import Politik from './pages/News/Politik';
-import Olahraga from './pages/News/Olahraga';
-import Hiburan from './pages/News/Hiburan';
 import Profile from './pages/Auth/Profile';
 import CreateNews from './pages/CreateNews';
 import AdminDashboard from './pages/Admin/AdminDashboard';
@@ -28,10 +24,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/bookmark" element={<Bookmark />} />
-        <Route path="/kategori/teknologi" element={<Teknologi />} />
-        <Route path="/kategori/politik" element={<Politik />} />
-        <Route path="/kategori/olahraga" element={<Olahraga />} />
-        <Route path="/kategori/hiburan" element={<Hiburan />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/create-news" element={<CreateNews /> } />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
